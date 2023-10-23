c8d128f7dec6b1158499f12c4eeaa1782cdd11f3
FrozenCell239 <val8ioks@gmail.com>
2023-10-24 01:58:01 +0200

    Added the possibilties to add a new product and modify an existing product.

--- src/Controller/AdminPanel/AdmProductsController.php
+++ src/Controller/AdminPanel/AdmProductsController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\AdminPanel;
 
 use App\Entity\Product;
+use App\Form\ProductFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/products', name: 'app_admin_products_')]
 class AdmProductsController extends AbstractController
@@ -16,16 +20,51 @@ class AdmProductsController extends AbstractController
         return $this->render('admin/products/index.html.twig');
     }
     #[Route('/add', name: 'add')]
-    public function add(): Response
+    public function add(
+        Request $request,
+        EntityManagerInterface $entity_manager,
+        SluggerInterface $slugger
+    ): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/products/index.html.twig');
+        $product = new Product(); //Creating the new product.
+        $product_form = $this->createForm(ProductFormType::class, $product); //Creating the new product's form.
+        $product_form->handleRequest($request); //Handling the the form request.
+        if($product_form->isSubmitted() && $product_form->isValid()){ //Checking if the form is both submitted and valid.
+            $product
+                ->setSlug($slugger->slug($product->getName())) //Creating the slug.
+                ->setPrice($product->getPrice() * 100) //Fixing the price.
+            ;
+            $entity_manager->persist($product);
+            $entity_manager->flush();
+            $this->addFlash('success', 'Produit ajouté avec succès !');
+            return $this->redirectToRoute('app_admin_products_index');
+        };
+        return $this->render('admin/products/add.html.twig', compact('product_form'));
     }
     #[Route('/edit/{id}', name: 'edit')]
-    public function edit(Product $product): Response
+    public function edit(
+        Product $product,
+        Request $request,
+        EntityManagerInterface $entity_manager,
+        SluggerInterface $slugger
+    ): Response
     {
         $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product); //Checking if user is allowed to edit with the voter.
-        return $this->render('admin/products/index.html.twig');
+        $product->setPrice($product->getPrice() / 100); //Fixing the price.
+        $product_form = $this->createForm(ProductFormType::class, $product); //Creating the new product's form.
+        $product_form->handleRequest($request); //Handling the the form request.
+        if($product_form->isSubmitted() && $product_form->isValid()){ //Checking if the form is both submitted and valid.
+            $product
+                ->setSlug($slugger->slug($product->getName())) //Creating the slug.
+                ->setPrice($product->getPrice() * 100) //Fixing the price.
+            ;
+            $entity_manager->persist($product);
+            $entity_manager->flush();
+            $this->addFlash('success', 'Produit modifié avec succès !');
+            return $this->redirectToRoute('app_admin_products_index');
+        };
+        return $this->render('admin/products/edit.html.twig', compact('product_form'));
     }
     #[Route('/delete/{id}', name: 'delete')]
     public function delete(Product $product): Response
@@ -34,9 +73,3 @@ class AdmProductsController extends AbstractController
         return $this->render('admin/products/index.html.twig');
     }
 }
-
-/**
- * NOTICE :
- * ROLE_PRODUCT_ADMIN peut modifier ou supprimer un produit, mais pas en ajouter.
- * Seul ROLE_ADMIN peut ajouter un produit.
- */
